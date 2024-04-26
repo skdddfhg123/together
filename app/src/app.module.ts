7eab8413a2d37b7b44d54332ee4dd89a8228a173
skdddfhg123 <skdddfhg6@gmail.com>
2024-04-26 14:22:07 +0900

    docker nest, postgres 성공

---
+++ app/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './config/typeorm.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ app/src/config/typeorm.config.ts
@@ -0,0 +1,29 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import * as config from 'config';
+
+// const dbConfig = config.get('db');
+// console.log('db :', dbConfig);
+
+// export const typeORMConfig : TypeOrmModuleOptions = {
+//     type: 'postgres',
+//     host: process.env.DB_HOSTNAME || dbConfig.host,
+//     port: process.env.DB_PORT || dbConfig.port,
+//     username: process.env.DB_USERNAME || dbConfig.username,
+//     password: process.env.DB_PASSWORD || dbConfig.password,
+//     database: process.env.DB_DATABASE || dbConfig.database,
+//     entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//     synchronize: dbConfig.synchronize
+// }
+export const typeORMConfig : TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOSTNAME,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+}
+
+console.log(typeORMConfig);
+
