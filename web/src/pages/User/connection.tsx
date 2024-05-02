b14c919a2e342c368e0ef4141c03b7d39c321aac
sjlim32 <32sjlim@gmail.com>
2024-05-02 13:23:17 +0900

    Web - user 라우팅 페이지 메인 이동버튼 추가

--- web/src/pages/User/connection.tsx
+++ web/src/pages/User/connection.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as KAKAKO from '@services/KakaoAPI';
+import { NavLink } from 'react-router-dom';
 
 export default function ConnectionPage() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1>동기화 관리 페이지</h1>
       <button onClick={KAKAKO.LogIn}>카카오톡 로그인</button>
+      <button onClick={KAKAKO.LogOut}>카카오톡 로그아웃</button>
+      <NavLink to="/main">메인으로 가기</NavLink>
     </div>
   );
 }
