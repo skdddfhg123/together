d45efd4bed6a4dff60be6f2ad6c862433952cc26
sgchoo <choo19950306@gmail.com>
2024-05-07 23:21:28 +0900

    fix module code

--- app/src/calendar/chat/chat.module.ts
+++ app/src/calendar/chat/chat.module.ts
@@ -3,20 +3,13 @@ import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entities/chat.entity';
-import { CalendarModule } from 'src/calendar/calendar.module';
-import { JWTStrategy } from 'src/auth/strategy/jwt.strategy';
-import { TokensModule } from 'src/db/tokens/tokens.module';
-
 @Module({
     imports: [
         // TypeOrmModule.forFeature([Chat]),
-        // CalendarModule,
-        // TokensModule,
     ],
     providers: [
         ChatGateway,
         ChatService,
-        // JWTStrategy,
     ],
 })
 export class ChatModule {}
